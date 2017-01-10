f373bc417042625d7121cb4245abbe5f764617e5
Erin Dachtler <download333@gmail.com>
2017-01-09 23:41:33 -0800

    Redirect to secure from insecure origin

--- src/client.js
+++ src/client.js
@@ -2,6 +2,10 @@ import {h, render} from 'preact';
 import Player from './components/player';
 
 
+if(location.protocol === 'http:') {
+  location.replace('https://' + location.host + location.pathname);
+}
+
 render(<Player/>, document.getElementById('player-mount'));
 
 if(navigator.serviceWorker) {
